0387efddddca11e52da874b6847776d5418e4f28
admin <admin@One2Ns-MacBook-Air.local>
2025-04-17 23:29:38 +0530

    Remove overhead of assertion from file

--- internal/adapter/writer/file_writer_test.go
+++ internal/adapter/writer/file_writer_test.go
@@ -1,15 +1,12 @@
 package writer
 
 import (
-	"bufio"
-	"os"
-	"reflect"
 	"testing"
 )
 
 func TestWriteSQL(t *testing.T) {
 	opFile := "/tmp/test.txt"
-	sqls := []string{"CREATE SCHEMA test;", "CREATE TABLE test.student (_id VARCHAR(255) PRIMARY KEY, date_of_birth VARCHAR(255), is_graduated BOOLEAN, name VARCHAR(255), roll_no FLOAT);"}
+	sqls := []string{"CREATE SCHEMA test; \n", "CREATE TABLE test.student (_id VARCHAR(255) PRIMARY KEY, date_of_birth VARCHAR(255), is_graduated BOOLEAN, name VARCHAR(255), roll_no FLOAT); \n"}
 	tests := []struct {
 		sqls           []string
 		outputFilePath string
@@ -29,20 +26,4 @@ func TestWriteSQL(t *testing.T) {
 		fileWriter.Close()
 	}
 
-	// Assert written sqls
-
-	file, _ := os.Open(opFile)
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	var lines []string
-
-	// Read line by line
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	if !reflect.DeepEqual(sqls, lines) {
-		t.Errorf("Expected %v, Got %v", sqls, lines)
-	}
 }
